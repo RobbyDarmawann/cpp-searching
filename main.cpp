a13c7e4f5612b68ec62798ad5dd836adf7d9ff7e
RobbyDarmawann <robbydrmawan@gmail.com>
2024-11-16 20:27:25 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> 
+using namespace std;
+
+struct Mahasiswa {
+    string NIM;
+    string Nama;
+    string Alamat;
+    string Kelas;
+    float Nilai;
+};
+
+int sequentialSearch(const vector<Mahasiswa>& data, const string& nama) {
+    for (int i = 0; i < data.size(); ++i) {
+        if (data[i].Nama == nama) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int binarySearch(const vector<Mahasiswa>& data, const string& nama) {
+    int left = 0, right = data.size() - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (data[mid].Nama == nama) {
+            return mid;
+        }
+        if (data[mid].Nama < nama) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1; 
+}
+
+void printMahasiswa(const Mahasiswa& mhs) {
+    cout << "NIM: " << mhs.NIM << endl;
+    cout << "Nama: " << mhs.Nama << endl;
+    cout << "Alamat: " << mhs.Alamat << endl;
+    cout << "Kelas: " << mhs.Kelas << endl;
+    cout << "Nilai: " << mhs.Nilai << endl;
+}
+
+int main() {
+    vector<Mahasiswa> mahasiswa = {
+        {"101", "Budi", "Jakarta", "A", 85.5},
+        {"102", "Ani", "Bandung", "B", 90.0},
+        {"103", "Tono", "Surabaya", "A", 78.0},
+        {"104", "Siti", "Medan", "C", 88.0},
+        {"105", "Andi", "Yogyakarta", "B", 82.5}
+    };
+
+    string cariNama;
+    cout << "Masukkan nama mahasiswa yang dicari: ";
+    cin >> cariNama;
+
+    int indexSeq = sequentialSearch(mahasiswa, cariNama);
+    if (indexSeq != -1) {
+        cout << "\nDitemukan (Sequential Search):" << endl;
+        printMahasiswa(mahasiswa[indexSeq]);
+    } else {
+        cout << "\nData tidak ditemukan (Sequential Search)." << endl;
+    }
+
+    sort(mahasiswa.begin(), mahasiswa.end(), [](const Mahasiswa& a, const Mahasiswa& b) {
+        return a.Nama < b.Nama;
+    });
+
+    int indexBin = binarySearch(mahasiswa, cariNama);
+    if (indexBin != -1) {
+        cout << "\nDitemukan (Binary Search):" << endl;
+        printMahasiswa(mahasiswa[indexBin]);
+    } else {
+        cout << "\nData tidak ditemukan (Binary Search)." << endl;
+    }
+
+    return 0;
+}
